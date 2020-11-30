873a07ee11da9c06447b5234a6660ee0d3a8b56d
Nguyen Trung Dung <trungdung.nguyen@vmodev.com>
2020-11-30 17:23:28 +0700

    update view

--- public/xuly0.js
+++ public/xuly0.js
@@ -1,21 +1,22 @@
 var socket = io("https://maca-chat-app.herokuapp.com/");
-socket.on("Server-send-room",function(data) {
+socket.on("Server-send-room", function (data) {
   $("#list-room").html("");
-  data.map(function(r){
-    $("#list-room").append("<h4 class='room'>"+ r +"</h4>")
+  data.map(function (r) {
+    $("#list-room").append("<h4 class='room'>" + r + "</h4>")
   })
 });
-socket.on("Server-send-room-name",function(data){
+socket.on("Server-send-room-name", function (data) {
   $("#Room-now").html(data);
 })
-socket.on("server-chat",function(data){
-  $("#right").append("<div>"+ data + "</div>")
+socket.on("server-chat", function (data) {
+  $("#right").append("<div>" + data + "</div>")
 })
-    $(document).ready(function(){
-    $("#btnCreate").click(function(){
-      socket.emit("Create-Room",$("#txtUsername").val());
-    });
-    $("#btnSend").click(function(){
-      socket.emit("user-chat",$("#txtMessage").val());
-    });
-    });
+$(document).ready(function () {
+  $("#btnCreate").click(function () {
+    socket.emit("Create-Room", $("#txtUsername").val());
+  });
+  $("#btnSend").click(function () {
+    socket.emit("user-chat", $("#txtMessage").val());
+  });
+});
+$('div#listMessages').scrollTop($('div#listMessages')[0].scrollHeight)
