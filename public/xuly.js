d36921325db8a887087cc98b46b96e012fbba462
Nguyen Trung Dung <trungdung.nguyen@vmodev.com>
2020-11-30 17:02:15 +0700

    update view

--- public/xuly.js
+++ public/xuly.js
@@ -1,50 +1,51 @@
 var socket = io("https://maca-chat-app.herokuapp.com/");
-socket.on("server-regist-fail",function(){
+socket.on("server-regist-fail", function () {
   alert("Username have registed")
 });
-socket.on("Server-regist-success",function(data){
+socket.on("Server-regist-success", function (data) {
   $("#currentUser").html(data);
   $("#loginForm").hide(2000);
   $("#chatForm").show(1000);
 });
-socket.on("Sever-send-username-all",function(data){
+socket.on("Sever-send-username-all", function (data) {
   $("#boxContent").html("");
   data.forEach(function (i) {
     $("#boxContent").append("<div class='user'>" + i + "</div>");
   });
 });
-socket.on("Server-send-message",function(data){
-  $("#listMessages").append("<div class='ms'>"+ data.un + ":" + data.nd+"</div>")
+socket.on("Server-send-message", function (data) {
+  $("#listMessages").append("<div class='ms'>" + data.un + ":" + data.nd + "</div>")
 });
-socket.on("sb-typing",function(data){
+socket.on("sb-typing", function (data) {
   $("#thongbao").html("<img width='20px' src='images.png'>" + data);
 });
-socket.on("sb-none-typing",function(data){
+socket.on("sb-none-typing", function (data) {
   $("#thongbao").html("");
 });
-    $(document).ready(function(){
-    $("#loginForm").show();
-    $("#chatForm").hide();
-    $("#txtMessage").focusin(function(){
-      socket.emit("typing");
-    });
-    $("#txtMessage").focusout(function(){
-      socket.emit("none-typing");
-    })
-    $("#btnRegister").click(function(){
-      socket.emit("client-send-Username",$("#txtUsername").val());
-    });
-    $("#btnLogout").click(function(){
-      socket.emit("logout");
-      $("#loginForm").show(1000);
-      $("#chatForm").hide(2000);
-    });
-    $(document).on('keypress',function(e) {
-      if(e.which == 13) {
-           socket.emit("user-send-message",$("#txtMessage").val());
-      }
+$(document).ready(function () {
+  $("#loginForm").show();
+  $("#chatForm").hide();
+  $("#txtMessage").focusin(function () {
+    socket.emit("typing");
   });
-    $("#btSendMessage").click(function(){
-      socket.emit("user-send-message",$("#txtMessage").val());
-    });
-    });
+  $("#txtMessage").focusout(function () {
+    socket.emit("none-typing");
+  })
+  $("#btnRegister").click(function () {
+    socket.emit("client-send-Username", $("#txtUsername").val());
+  });
+  $("#btnLogout").click(function () {
+    socket.emit("logout");
+    $("#loginForm").show(1000);
+    $("#chatForm").hide(2000);
+  });
+  $(document).on('keypress', function (e) {
+    if (e.which == 13) {
+      socket.emit("user-send-message", $("#txtMessage").val());
+    }
+  });
+  $("#btSendMessage").click(function () {
+    socket.emit("user-send-message", $("#txtMessage").val());
+  });
+});
+$('div#listMessages').scrollTop($('div#listMessages')[0].scrollHeight)
